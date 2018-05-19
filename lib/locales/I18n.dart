0ab49b9015bea3dc76f7c80886e3b2d8240e1a98
Hai Vu <vietnam5583@gmail.com>
2018-05-20 00:44:35 +0700

    Fix checkout t function

--- lib/locales/I18n.dart
+++ lib/locales/I18n.dart
@@ -5,19 +5,16 @@ class I18n {
   String language = 'vi';
   Map<String, dynamic> languageData;
 
-  I18n(String language){
+  I18n(String language) {
     this.language = language;
     this.languageData = languages[language];
   }
 
-  t(key){
-    if (this.languageData[key] == null) return '';
-    return this.languageData[key];
-  }
+  t(key) => this.languageData[key] ?? '';
 
-  static getInstance([String language = 'vi']){
+  static getInstance([String language = 'vi']) {
     print(languages);
-    if (_instance == null){
+    if (_instance == null) {
       _instance = new I18n(language);
     }
     return _instance;
