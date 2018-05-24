d5d434952a5c48f54cf37a566fd46307d9a3009d
Hai Vu <vietnam5583@gmail.com>
2018-05-24 17:03:22 +0700

    Configure Facebook SDK

---
+++ lib/api/constants.dart
@@ -0,0 +1,3 @@
+final String PREPRODUCTION_SERVER_ADDRESS = "https://betav2.clingme.vn:19000";
+final String DEVELOPMENT_SERVER_ADDRESS = "https://pre-production-v2.clingme.vn";
+final String SERVER_URL_ADDRESS = "https://mobi2.clingme.vn";
--- lib/containers/login.dart
+++ lib/containers/login.dart
@@ -11,10 +11,20 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   bool _loadingLogin = false;
 
-  _handleLoginFacebook () async{
+  _handleLoginFacebook() async {
     var facebookLogin = new FacebookLogin();
     var result = await facebookLogin.logInWithReadPermissions(['email']);
-    print(result.toString());
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        print('Logged In: '+ result.accessToken.token);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print('User Cancel');
+        break;
+      case FacebookLoginStatus.error:
+        print('Error: ' + result.errorMessage);
+        break;
+    }
   }
 
   Widget build(BuildContext context) {
@@ -28,77 +38,74 @@ class _LoginState extends State<Login> {
               fit: BoxFit.cover),
         ),
         child: new Padding(
-          padding: new EdgeInsets.all(32.0),
-          child: new Column(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              new SizedBox(height: 50.0),
-              new Image.asset(
+            padding: new EdgeInsets.all(32.0),
+            child: new Center(
+                child: new Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                new SizedBox(height: 50.0),
+                new Image.asset(
                   'resources/images/logo.png',
                   width: 100.0,
                   height: 70.0,
-              ),
-
-              new TextFormField(
-                decoration: const InputDecoration(
-                    border: const UnderlineInputBorder(),
-                    filled: true,
-                    labelText: 'Tên đăng nhập',
-                    fillColor: Colors.white),
-              ),
-              const SizedBox(height: 20.0),
-              new TextFormField(
-                decoration: const InputDecoration(
-                    border: const UnderlineInputBorder(),
-                    filled: true,
-                    labelText: 'Mật khẩu',
-                    fillColor: Colors.white),
-              ),
-              const SizedBox(height: 20.0),
-              new Row(children: <Widget>[
-                new Expanded(
-                    child: new FlatButton(
-                  onPressed: () {
-                    this.setState(() {
-                      _loadingLogin = !_loadingLogin;
-                    });
-                  },
-                  color: _loadingLogin ? Colors.black12 : Color(0xFFF16654),
-                  child: _loadingLogin
-                      ? new CircularProgressIndicator(
-                          backgroundColor: Color(0xFFF16654),
-
-                      )
-                      : new Text(
-                          I18n.getInstance().t('login'),
-                          style: new TextStyle(color: Colors.white),
-                        ),
-                ))
-              ]),
-
-              const SizedBox(height: 12.0),
-              new Container(
-                child: new Column(
-                    children: <Widget>[new Text(I18n.getInstance().t('or'))]),
-              ),
-              const SizedBox(height: 12.0),
-              new Row(children: <Widget>[
-                new Expanded(
-                    child: new FlatButton.icon(
-                        color: Color(0xFF3B5998),
-                        onPressed: _handleLoginFacebook,
-                        icon: new Icon(
-                            IconData(0xe945, fontFamily: 'icon-clingme-2-0'),
-                            size: 24.0,
-                            color: Colors.white),
-                        label: new Text(
-                          I18n.getInstance().t('login_with_facebook'),
-                          style: new TextStyle(color: Colors.white),
-                        )))
-              ])
-            ],
-          ),
-        ),
+                ),
+                new TextFormField(
+                  decoration: const InputDecoration(
+                      border: const UnderlineInputBorder(),
+                      filled: true,
+                      labelText: 'Tên đăng nhập',
+                      fillColor: Colors.white),
+                ),
+                const SizedBox(height: 20.0),
+                new TextFormField(
+                  decoration: const InputDecoration(
+                      border: const UnderlineInputBorder(),
+                      filled: true,
+                      labelText: 'Mật khẩu',
+                      fillColor: Colors.white),
+                ),
+                const SizedBox(height: 32.0),
+                new Row(children: <Widget>[
+                  new Expanded(
+                      child: new FlatButton(
+                    onPressed: () {
+                      this.setState(() {
+                        _loadingLogin = !_loadingLogin;
+                      });
+                    },
+                    color: _loadingLogin ? Colors.black12 : Color(0xFFF16654),
+                    child: _loadingLogin
+                        ? new CircularProgressIndicator(
+                            backgroundColor: Color(0xFFF16654),
+                          )
+                        : new Text(
+                            I18n.getInstance().t('login'),
+                            style: new TextStyle(color: Colors.white),
+                          ),
+                  ))
+                ]),
+                const SizedBox(height: 12.0),
+                new Container(
+                  child: new Column(
+                      children: <Widget>[new Text(I18n.getInstance().t('or'))]),
+                ),
+                const SizedBox(height: 12.0),
+                new Row(children: <Widget>[
+                  new Expanded(
+                      child: new FlatButton.icon(
+                          color: Color(0xFF3B5998),
+                          onPressed: _handleLoginFacebook,
+                          icon: new Icon(
+                              IconData(0xe945, fontFamily: 'icon-clingme-2-0'),
+                              size: 24.0,
+                              color: Colors.white),
+                          label: new Text(
+                            I18n.getInstance().t('login_with_facebook'),
+                            style: new TextStyle(color: Colors.white),
+                          )))
+                ])
+              ],
+            ))),
       ),
     );
   }
